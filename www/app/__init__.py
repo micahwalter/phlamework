98ca5a8460912469b381aecd78ad09557c7586d0
Micah Walter <micah@micahwalter.com>
2015-10-21 15:38:58 -0400

    conf stuff

--- www/app/__init__.py
+++ www/app/__init__.py
@@ -3,17 +3,19 @@ import MySQLdb
 import MySQLdb.cursors
 
 app = Flask(__name__)
-app.debug = True
+app.debug = True ### Set this to false for production apps
 
 ## global config stuff ...
 app.cfg = {}
 
 app.cfg['auth_cookie_name'] = 'a'
-app.cfg['crypto_cookie_secret'] = 'XWvXBujpxWtm9YYl3Kt2xo4rqp_wEXKB8GI1CxZJ4wg='
 app.cfg['auth_cookie_domain'] = 'localhost'
 
-app.cfg['enable_feature_signin'] = 1;
+### Important!!!  -- Use bin/generate_secret.py to make a crypto key of your own.
+app.cfg['crypto_cookie_secret'] = 'XWvXBujpxWtm9YYl3Kt2xo4rqp_wEXKB8GI1CxZJ4wg='
 
+
+### Database connnection info
 database = MySQLdb.connect(
     host = "localhost",
     user = "root",
@@ -21,8 +23,13 @@ database = MySQLdb.connect(
     db = "phlamework",
     cursorclass=MySQLdb.cursors.DictCursor)
 
+### Bootstrap the DB connection
 app.db = database.cursor()
 
+### Feature flags
+app.cfg['enable_feature_signin'] = 1;
+
+### Do these things on every page load
 from app.auth import login
 
 @app.before_request
@@ -30,8 +37,6 @@ def init():
     auth_cookie = request.cookies.get(app.cfg['auth_cookie_name'])
     login.check_login(auth_cookie)
 
-
-
-## import all the things...
+## import all the views...
 from pages import views
 from auth import views
